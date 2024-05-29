662112a4e374fd912d04bb1a1d9f9d43c5f0eeb0
Jane Plyasovskikh <125005421+plyasovskikh@users.noreply.github.com>
2024-05-29 21:04:27 +0400

    Homework about objects

---
+++ homework_objects.js
@@ -0,0 +1,54 @@
+// ДЗ
+
+// 1. Обращение к свойству объекта: Создайте объект person с свойствами name и age, затем напишите код, который обращается к свойству name объекта person и выводит его значение.
+
+const user = {
+
+    firstname: 'Jane',
+    lastname: 'Plyasovskikh',
+    age: 26
+}
+console.log(user.age)
+
+// 2. Проверка наличия свойства: Напишите функцию, которая принимает объект и имя свойства, и возвращает true, если это свойство существует в объекте, и false в противном случае.
+
+function hasProperty(obj, propName) {
+    return propName in obj;
+}
+console.log(hasProperty(user, 'sex')) //false
+console.log(hasProperty(user, 'age')) //true
+
+// 3. Изменение свойства объекта: Напишите код, который изменяет значение свойства age объекта person на новое значение.
+
+user.age = 30;
+console.log(user.age);
+
+// 4. Добавление нового свойства: Напишите код, который добавляет новое свойство gender со значением "male" к объекту person.
+
+user.gender = 'male'
+console.log(user.gender)
+console.log(user)
+
+// 5. Удаление свойства: Напишите код, который удаляет свойство age из объекта person.
+
+delete user.age
+console.log(user)
+
+// 6. Обращение к вложенным свойствам: Создайте объект car с вложенными свойствами engine и wheels, затем напишите код, который обращается к свойству engine объекта car и выводит его значение.
+
+// Создание объекта car с вложенными свойствами engine и wheels
+const car = {
+    engine: {
+        type: 'V8',
+        horsepower: 450
+    },
+    wheels: {
+        count: 4,
+        size: 18
+    }
+};
+
+// Обращение к свойству engine и вывод его значения
+console.log(car.engine); // { type: 'V8', horsepower: 450 }
+console.log(car.engine.type); // 'V8'
+console.log(car.engine.horsepower); // 450
